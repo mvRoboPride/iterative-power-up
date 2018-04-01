b19ca48821d5bdf6aeec87ab6ca3c963e31a4a52
Jasmine Gormley <19gormleyj@mvsdpride.org>
2018-04-01 16:48:03 -0400

    created auto timer, changed from robotdrive to differentialdrive

--- iterative power up/src/org/usfirst/frc/team6690/robot/Robot.java
+++ iterative power up/src/org/usfirst/frc/team6690/robot/Robot.java
@@ -12,301 +12,309 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-@SuppressWarnings({ "unused", "deprecation" })
+@SuppressWarnings("unused")
 public class Robot extends IterativeRobot implements PIDOutput {
+
+	DifferentialDrive myDrive;
+
+	// Joystick driveStick;
+	// Joystick liftStick = new Joystick(1);
+
+	XboxController xbox;
+
+	Compressor comp = new Compressor(0);
+	Solenoid solenoid = new Solenoid(0);
+	Solenoid sol2 = new Solenoid(1);
+
+	Spark liftSpark = new Spark(5);
+	Spark endSpark = new Spark(0);
+	Spark endSpark2 = new Spark(6);
+	Spark leftDrive1 = new Spark(3);
+	Spark leftDrive2 = new Spark(4);
+	Spark rightDrive1 = new Spark(1);
+	Spark rightDrive2 = new Spark(2);
+
+	SpeedControllerGroup leftDriveSide = new SpeedControllerGroup(leftDrive1, leftDrive2);
+	SpeedControllerGroup rightDriveSide = new SpeedControllerGroup(rightDrive1, rightDrive2);
+
+	Timer autoTimer = new Timer();
 	
-		RobotDrive myDrive;
-		
-		Joystick driveStick;
-		Joystick liftStick = new Joystick(1);
-		
-		XboxController xbox;
-		
-		Compressor comp = new Compressor(0);
-		Solenoid solenoid = new Solenoid(0);
-		
-		Spark liftSpark = new Spark(5);
-		Spark endSpark = new Spark(0);
-		public static int gyroPort = 1;
-		
-		
-	    public static final double Kp = .2;
-	    public static final double Ki = .2;
-	    public static final double Kd = .2;
-	    
-	    /* Tolerance, or how many degrees the robot can be off it's heading. */
-	    public static final double kTolerance = 2f;
-	    
-	    public static PIDController turnController;
+	DigitalInput endSwitch;
+	
+	PowerDistributionPanel pdp = new PowerDistributionPanel();
 
-		
-		DigitalInput endSwitch;
-		private ADXRS450_Gyro gyro;
-		PowerDistributionPanel pdp = new PowerDistributionPanel();
-		
-		double rotateToAngle;
-		
-		
-		final String baseline = "Baseline";
-		final String leftAuto = "Left Auto";
-		final String rightAuto = "Right Auto";
-		final String centerAuto = "Center Auto";
-		String autoSelected;
-		SendableChooser<String> chooser = new SendableChooser<>();
-		
-		
+	final String baseline = "Baseline";
+	final String leftAuto = "Left Auto";
+	final String rightAuto = "Right Auto";
+	final String centerAuto = "Center Auto";
+	String autoSelected;
+	SendableChooser<String> chooser = new SendableChooser<>();
 
-		
 	@Override
 	public void robotInit() {
-		
-		comp.setClosedLoopControl(true);
+
 		solenoid.set(false);
-		
-		turnController = new PIDController(Kp, Ki, Kd, gyro, (PIDOutput) myDrive);
-		turnController.setInputRange(-180f, 180f); /* Input from gyro with wrapping */
-		turnController.setOutputRange(-1, 1); /* Max and min voltage for motors */
-		turnController.setAbsoluteTolerance(kTolerance);
-		turnController.setContinuous(true);
-		turnController.disable();
-		
-		UsbCamera Camera = CameraServer.getInstance().startAutomaticCapture(0); //usb camera
+
+		UsbCamera Camera = CameraServer.getInstance().startAutomaticCapture(0); // usb camera
 		Camera.setFPS(35);
 		Camera.setResolution(640, 640);
-		
-		pdp.clearStickyFaults();
-		
-		myDrive = new RobotDrive(1,2,3,4); //Arcade Drive
-    	driveStick = new Joystick(1); //Arcade Drive
-    	
-    	xbox = new XboxController(1);
-    	
-    	endSwitch = new DigitalInput(0);
-    	
-    	ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-    	gyro.reset();
 
-    	
-    	chooser.addDefault("Baseline", baseline);
+		pdp.clearStickyFaults();
+
+		myDrive = new DifferentialDrive(leftDriveSide, rightDriveSide);// (1,2,3,4); //Arcade Drive
+		// driveStick = new Joystick(1); // Arcade Drive
+
+		xbox = new XboxController(1);
+
+		endSwitch = new DigitalInput(0);
+
+		chooser.addDefault("Baseline", baseline);
 		chooser.addObject("Center Auto", centerAuto);
 		chooser.addObject("Left Auto", leftAuto);
 		chooser.addObject("Right Auto", rightAuto);
 		SmartDashboard.putData("Auto choices", chooser);
-	    
-		
+		System.out.println("Robot initialized");
 	}
 
 	@Override
-	public void autonomousInit() { //11.75 second full climb
-		turnController.setSetpoint(90.0f);
-		turnController.enable();
-      /* 	autoSelected = chooser.getSelected();
-		System.out.println("Auto selected: " + autoSelected);
+	public void autonomousInit() { // 11.75 second full climb
 		
+		System.out.println("starting auto");
+		autoTimer.reset();
+		autoTimer.start();
+		autoSelected = chooser.getSelected();
+		System.out.println("Auto selected: " + autoSelected);
+
 		String gameData;
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
 		switch (autoSelected) {
 		case leftAuto:
-        if(gameData.length() > 0)
-                { 
-		  if(gameData.charAt(0) == 'L')
-		  {
-			myDrive.arcadeDrive(-1, .45); //drives straight
-			Timer.delay(7);
-			myDrive.arcadeDrive(-1, 1); //turns
-			Timer.delay(2);
-			myDrive.arcadeDrive(0, 0);
-			liftSpark.set(1);//raises lift
-			Timer.delay(5);
-			liftSpark.set(0);
-			endSpark.set(1);//launches cube
-			Timer.delay(.5);
-			endSpark.set(0);
-			liftSpark.set(-1); //lowers lift
-			Timer.delay(4);
-			liftSpark.set(0);
-			
-		  } else if(gameData.charAt(0) == 'R') { // right  
-				myDrive.arcadeDrive(-1, .45); //drives straight
-				Timer.delay(7);
-				myDrive.arcadeDrive(-1, 1); //turns
-				Timer.delay(3);
-				myDrive.arcadeDrive(0, 0);
-				myDrive.arcadeDrive(-1, .45); //straight
-				Timer.delay(6);
-				myDrive.arcadeDrive(-1, 1); //turns
-				Timer.delay(2);
-				myDrive.arcadeDrive(-1, 1);//straight
-				Timer.delay(1);
-				myDrive.arcadeDrive(0, 0);
-				liftSpark.set(1);//raises lift
-				Timer.delay(5);
-				liftSpark.set(0);
-				endSpark.set(1);//launches cube
-				Timer.delay(.5);
-				endSpark.set(0);
-				liftSpark.set(-1); //lowers lift
-				Timer.delay(4);
-				liftSpark.set(0);
-		  }
-			break;
-                }
-		case centerAuto: 
-			   if(gameData.length() > 0)
-               {
-		  if(gameData.charAt(0) == 'L')
-		  {
-				myDrive.arcadeDrive(-1, -1); //turns
-				Timer.delay(2);
-				myDrive.arcadeDrive(-1, .45); //straight
-				Timer.delay(4);
-				myDrive.arcadeDrive(-1, 1); //turns
-				Timer.delay(1);
-				myDrive.arcadeDrive(-1, .45);//straight
-				Timer.delay(2);
-				myDrive.arcadeDrive(-1, 1); //turns
-				Timer.delay(1);
-				myDrive.arcadeDrive(-1, .45); //straight
-				Timer.delay(1);
-				myDrive.arcadeDrive(0, 0);
-				liftSpark.set(1);//raises lift
-				Timer.delay(4);
-				liftSpark.set(0);
-				endSpark.set(1);//launches cube
-				Timer.delay(.5);
-				endSpark.set(0);
-				liftSpark.set(-1); //lowers lift
-				Timer.delay(4);
-				liftSpark.set(0);
-			
-		  } else if(gameData.charAt(0) == 'R') {
-			  myDrive.arcadeDrive(-1, 1); //turns
-				Timer.delay(2);
-				myDrive.arcadeDrive(-1, .45); //straight
-				Timer.delay(4);
-				myDrive.arcadeDrive(-1, -1); //turns
-				Timer.delay(1);
-				myDrive.arcadeDrive(-1, .45);//straight
-				Timer.delay(2);
-				myDrive.arcadeDrive(-1, -1); //turns
-				Timer.delay(1);
-				myDrive.arcadeDrive(-1, .45); //straight
-				Timer.delay(1);
-				myDrive.arcadeDrive(0, 0);
-				liftSpark.set(1);//raises lift
-				Timer.delay(4);
-				liftSpark.set(0);
-				endSpark.set(1);//launches cube
-				Timer.delay(.5);
-				endSpark.set(0);
-				liftSpark.set(-1); //lowers lift
-				Timer.delay(4);
-				liftSpark.set(0);
+			if (gameData.length() > 0) {
+				if (gameData.charAt(0) == 'L') {
+					myDrive.arcadeDrive(-1, .45); // drives straight
+					Timer.delay(7);
+					myDrive.arcadeDrive(-1, 1); // turns
+					Timer.delay(2);
+					myDrive.arcadeDrive(0, 0);
+					liftSpark.set(1);// raises lift
+					Timer.delay(5);
+					liftSpark.set(0);
+					endSpark.set(1);// launches cube
+					Timer.delay(.5);
+					endSpark.set(0);
+					liftSpark.set(-1); // lowers lift
+					Timer.delay(4);
+					liftSpark.set(0);
+
+				} else if (gameData.charAt(0) == 'R') { // right
+					myDrive.arcadeDrive(-1, .45); // drives straight
+					Timer.delay(7);
+					myDrive.arcadeDrive(-1, 1); // turns
+					Timer.delay(3);
+					myDrive.arcadeDrive(0, 0);
+					myDrive.arcadeDrive(-1, .45); // straight
+					Timer.delay(6);
+					myDrive.arcadeDrive(-1, 1); // turns
+					Timer.delay(2);
+					myDrive.arcadeDrive(-1, 1);// straight
+					Timer.delay(1);
+					myDrive.arcadeDrive(0, 0);
+					liftSpark.set(1);// raises lift
+					Timer.delay(5);
+					liftSpark.set(0);
+					endSpark.set(1);// launches cube
+					Timer.delay(.5);
+					endSpark.set(0);
+					liftSpark.set(-1); // lowers lift
+					Timer.delay(4);
+					liftSpark.set(0);
 				}
-			break;
-               }
-		case rightAuto: 
-			   if(gameData.length() > 0)
-               {
-		  if(gameData.charAt(0) == 'L')
-		  {
-				myDrive.arcadeDrive(-1, .45); //drives straight
-				Timer.delay(10);
-				myDrive.arcadeDrive(-1, -1); //turns
-				Timer.delay(3);
-				myDrive.arcadeDrive(0, 0);
-				myDrive.arcadeDrive(-1, .45); //straight
-				Timer.delay(9);
-				myDrive.arcadeDrive(-1, -1); //turns
-				Timer.delay(3);
-				myDrive.arcadeDrive(-1, 1);//straight
-				Timer.delay(1);
-				myDrive.arcadeDrive(0, 0);
-				liftSpark.set(1);//raises lift
-				Timer.delay(5);
-				liftSpark.set(0);
-				endSpark.set(1);//launches cube
-				Timer.delay(.5);
-				endSpark.set(0);
-				liftSpark.set(-1); //lowers lift
-				Timer.delay(4);
-				liftSpark.set(0);
-			
-		  } else if(gameData.charAt(0) == 'R')  {
-			  myDrive.arcadeDrive(-1, .45); //drives straight
-				Timer.delay(10);
-				myDrive.arcadeDrive(-1, -1); //turns
-				Timer.delay(3);
-				myDrive.arcadeDrive(0, 0);
-				liftSpark.set(1);//raises lift
-				Timer.delay(4);
-				liftSpark.set(0);
-				endSpark.set(1);//launches cube
-				Timer.delay(.5);
-				endSpark.set(0);
-				liftSpark.set(-1);
-				Timer.delay(4);
-				liftSpark.set(0);		  
+				break;
+			}
+		case centerAuto:
+			if (gameData.length() > 0) {
+				if (gameData.charAt(0) == 'L') {
+					myDrive.arcadeDrive(-1, -1); // turns
+					Timer.delay(2);
+					myDrive.arcadeDrive(-1, .45); // straight
+					Timer.delay(4);
+					myDrive.arcadeDrive(-1, 1); // turns
+					Timer.delay(1);
+					myDrive.arcadeDrive(-1, .45);// straight
+					Timer.delay(2);
+					myDrive.arcadeDrive(-1, 1); // turns
+					Timer.delay(1);
+					myDrive.arcadeDrive(-1, .45); // straight
+					Timer.delay(1);
+					myDrive.arcadeDrive(0, 0);
+					liftSpark.set(1);// raises lift
+					Timer.delay(4);
+					liftSpark.set(0);
+					endSpark.set(1);// launches cube
+					Timer.delay(.5);
+					endSpark.set(0);
+					liftSpark.set(-1); // lowers lift
+					Timer.delay(4);
+					liftSpark.set(0);
+
+				} else if (gameData.charAt(0) == 'R') {
+					myDrive.arcadeDrive(-1, 1); // turns
+					Timer.delay(2);
+					myDrive.arcadeDrive(-1, .45); // straight
+					Timer.delay(4);
+					myDrive.arcadeDrive(-1, -1); // turns
+					Timer.delay(1);
+					myDrive.arcadeDrive(-1, .45);// straight
+					Timer.delay(2);
+					myDrive.arcadeDrive(-1, -1); // turns
+					Timer.delay(1);
+					myDrive.arcadeDrive(-1, .45); // straight
+					Timer.delay(1);
+					myDrive.arcadeDrive(0, 0);
+					liftSpark.set(1);// raises lift
+					Timer.delay(4);
+					liftSpark.set(0);
+					endSpark.set(1);// launches cube
+					Timer.delay(.5);
+					endSpark.set(0);
+					liftSpark.set(-1); // lowers lift
+					Timer.delay(4);
+					liftSpark.set(0);
 				}
-			break;
-               }
-		case baseline:
+				break;
+			}
+		case rightAuto:
+			if (gameData.length() > 0) {
+				if (gameData.charAt(0) == 'L') {
+					myDrive.arcadeDrive(-1, .45); // drives straight
+					Timer.delay(10);
+					myDrive.arcadeDrive(-1, -1); // turns
+					Timer.delay(3);
+					myDrive.arcadeDrive(0, 0);
+					myDrive.arcadeDrive(-1, .45); // straight
+					Timer.delay(9);
+					myDrive.arcadeDrive(-1, -1); // turns
+					Timer.delay(3);
+					myDrive.arcadeDrive(-1, 1);// straight
+					Timer.delay(1);
+					myDrive.arcadeDrive(0, 0);
+					liftSpark.set(1);// raises lift
+					Timer.delay(5);
+					liftSpark.set(0);
+					endSpark.set(1);// launches cube
+					Timer.delay(.5);
+					endSpark.set(0);
+					liftSpark.set(-1); // lowers lift
+					Timer.delay(4);
+					liftSpark.set(0);
+
+				} else if (gameData.charAt(0) == 'R') {
+					myDrive.arcadeDrive(-1, .45); // drives straight
+					Timer.delay(10);
+					myDrive.arcadeDrive(-1, -1); // turns
+					Timer.delay(3);
+					myDrive.arcadeDrive(0, 0);
+					liftSpark.set(1);// raises lift
+					Timer.delay(4);
+					liftSpark.set(0);
+					endSpark.set(1);// launches cube
+					Timer.delay(.5);
+					endSpark.set(0);
+					liftSpark.set(-1);
+					Timer.delay(4);
+					liftSpark.set(0);
+				}
+				break;
+			}
 		default:
-			myDrive.arcadeDrive(-1, .45);
-			Timer.delay(10);
+			myDrive.arcadeDrive(-.5, .3);
+			Timer.delay(10.0);
 			myDrive.arcadeDrive(0, 0);
 			break;
-          } */
-       }
+		}
+	}
 
 	@Override
 	public void autonomousPeriodic() {
-	
-			
+		if (autoTimer.get() < 3.0) {
+			myDrive.arcadeDrive(.75, .3);
+		} else {
+			myDrive.arcadeDrive(0, 0);
 		}
-	//}
+	}
 
+	@Override
+	public void teleopInit() {
+		autoTimer.stop();
+	}
 
+	@Override
 	public void teleopPeriodic() {
-		
-		{while (isOperatorControl() && isEnabled()) {
-    		myDrive.arcadeDrive(driveStick);
-    		Timer.delay(0.01);
-   
-    		
-    		double rightStickValue = liftStick.getRawAxis(5);
-    		liftSpark.set(rightStickValue);
-    		
-    		if (xbox.getAButtonPressed()) {
-    			solenoid.set(true);
-    		}
-    		if (xbox.getAButtonReleased()) {
-    			solenoid.set(false);
-    		} else {
-    			solenoid.set(false);
-    		}
-    		if (xbox.getTriggerAxis(Hand.kRight) >= .05) {
+
+		while (isOperatorControl() && isEnabled()) {
+			myDrive.arcadeDrive(-xbox.getRawAxis(1), -xbox.getRawAxis(0));
+
+			comp.setClosedLoopControl(true);
+
+			double c1 = pdp.getCurrent(1);
+			double c2 = pdp.getCurrent(2);
+			double c3 = pdp.getCurrent(3);
+			double c4 = pdp.getCurrent(4);
+			double v = pdp.getVoltage();
+			SmartDashboard.putNumber("spark 1", c1);
+			SmartDashboard.putNumber("spark 2", c2);
+			SmartDashboard.putNumber("spark 3", c3);
+			SmartDashboard.putNumber("spark 4", c3);
+			SmartDashboard.putNumber("voltage", v);
+
+			double rightStickValue = xbox.getRawAxis(5);
+			liftSpark.set(rightStickValue);
+
+			if (endSwitch.get()) {
+				liftSpark.set(-1);
+				Timer.delay(.2);
+				liftSpark.set(0);
+			}
+			if (xbox.getAButton()) {
+				solenoid.set(true);
+			}
+			if (xbox.getBButton()) {
+				sol2.set(true);
+			}
+			if (xbox.getBButtonReleased()) {
+				sol2.set(false);
+			}
+			if (xbox.getAButtonReleased()) {
+				solenoid.set(false);
+
+			} else {
+				solenoid.set(false);
+			}
+			if (xbox.getTriggerAxis(Hand.kRight) >= .05) {
 				endSpark.set(xbox.getTriggerAxis(Hand.kRight));
-    		} else if (xbox.getTriggerAxis(Hand.kLeft) >= .05) {
-    			endSpark.set(xbox.getTriggerAxis(Hand.kLeft) * (-1));
-    		} else {
-    			endSpark.set(0);
-    		}
-	      Timer.delay(0.01); 
+				endSpark2.set(xbox.getTriggerAxis(Hand.kRight));
+			} else if (xbox.getTriggerAxis(Hand.kLeft) >= .05) {
+				endSpark.set(xbox.getTriggerAxis(Hand.kLeft) * (-1));
+				endSpark2.set(xbox.getTriggerAxis(Hand.kLeft) * (-1));
+			} else {
+				endSpark.set(0);
+				endSpark2.set(0);
 			}
 		}
 	}
@@ -314,8 +322,6 @@ public class Robot extends IterativeRobot implements PIDOutput {
 	@Override
 	public void pidWrite(double output) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
-		
-		
